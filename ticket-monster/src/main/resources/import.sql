ed83b58d87512b55dcd0f4e947675b592611d731
sumant <sumant@10.1.1.119>
2013-07-25 01:23:50 +1000

    Part II

--- ticket-monster/src/main/resources/import.sql
+++ ticket-monster/src/main/resources/import.sql
@@ -16,4 +16,9 @@
 --
 
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+-- insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+insert into Event (id, name, description, major, picture, version) 
+				values (1, 'Shane''s Sock Puppets', 'This critically acclaimed masterpiece...', true, 'http://dl.dropbox.com/u/65660684/640px-Carnival_Puppets.jpg', 1); 
+insert into Event (id, name, description, major, picture, version) 
+				values (2, 'Rock concert of the decade', 'Get ready to rock...', true, 'http://dl.dropbox.com/u/65660684/640px-Weir%2C_Bob_(2007)_2.jpg', 1); 
+-- See more at: http://www.jboss.org/jdf/examples/ticket-monster/tutorial/Introduction/#_reviewing_persistence_xml_amp_updating_import_sql
